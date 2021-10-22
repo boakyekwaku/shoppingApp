13a9a9f833c99ceb008a776944de9e340a200987
boakyekwaku <75320699+boakyekwaku@users.noreply.github.com>
2021-10-22 23:05:44 +0000

    Initial commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './screens/Home';
+
+
+export default function App() {
+
+  const MainNavigator = createStackNavigator();
+  return (
+    <View style={{flex: 1}}>
+        <NavigationContainer>
+          <MainNavigator.Navigator>
+            <MainNavigator.Screen name = "Home" component={Home} />
+            </MainNavigator.Navigator>
+        </NavigationContainer>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ screens/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Home ({navigation}){
+
+    return(
+        <View>
+            <Text>hjbhj</Text>
+        </View>)
+}
